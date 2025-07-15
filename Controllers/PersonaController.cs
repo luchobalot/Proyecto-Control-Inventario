5e7717ca449b8834972fca121acbe66c89c80f3a
luchobalot <lnbalot02@gmail.com>
2025-07-15 11:53:16 -0300

    Arreglo AutoMapper

---
+++ Controllers/PersonaController.cs
@@ -0,0 +1,13 @@
+﻿using Control.Models;
+using Control.Models.Dtos;
+using Control.Repositorio.IRepositorio;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Peliculas.Controllers
+{
+    
+   
+}
--- obj/Debug/net8.0/Control.AssemblyInfo.cs
+++ obj/Debug/net8.0/Control.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Control")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c69a65a8497a1801d0f1e6672180e21f8578344")]
 [assembly: System.Reflection.AssemblyProductAttribute("Control")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Control")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
